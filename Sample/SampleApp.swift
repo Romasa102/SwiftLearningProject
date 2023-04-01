f106ad8305ed6c47b018b6df751bcde89b8d8cb9
Romasa102 <masa.robot123@gmail.com>
2023-04-01 22:34:20 +0800

    Initial Commit

---
+++ Sample/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApp.swift
+//  Sample
+//
+//  Created by Manabe Masaaki on 2023/04/01.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
