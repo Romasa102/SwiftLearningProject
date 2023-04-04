cc3cc48752256e3a399f3da4ebf253053a5ba983
Romasa102 <masa.robot123@gmail.com>
2023-04-04 18:34:02 +0800

    Made a simple ui element to see how the swift work

--- Sample/ContentView.swift
+++ Sample/ContentView.swift
@@ -1,22 +1,64 @@
-//
-//  ContentView.swift
-//  Sample
-//
-//  Created by Manabe Masaaki on 2023/04/01.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var playerCard = "card5"
+    @State private var cpuCard = "card9"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Image("background").ignoresSafeArea()
+            VStack{
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                Button {
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    if(playerRand > cpuRand){
+                        playerScore+=1
+                    }else if(cpuRand > playerRand){
+                        cpuScore+=1
+                    }
+                } label: {
+                    Image("dealbutton")
+                }
+                Spacer()
+                
+                HStack{
+                    Spacer()
+                    VStack(spacing: 10.0){
+                        
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                        Text(String(playerScore)).font(.largeTitle).foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    VStack(spacing: 10.0){
+                        Text("CPU")
+                            .font(.headline).foregroundColor(Color.white)
+                        Text(String(cpuScore)).font(.largeTitle).foregroundColor(Color.white)
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
         }
-        .padding()
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
